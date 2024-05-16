75d436187f054658d6bbc4f8c42a9efacd5a1a5d
Steven Maude <git@stevenmaude.co.uk>
2024-05-16 13:33:43 +0100

    Check Python packages in dev container
    Check these are the same as the OpenSAFELY Python Docker image.

--- tests/dev_container.sh
+++ tests/dev_container.sh
@@ -7,6 +7,15 @@ set -euxo pipefail
 # Check the OpenSAFELY research-template example runs.
 opensafely run run_all
 
+# Check that the Python packages in the OpenSAFELY Python image are available in the dev container.
+# This checks package names and versions.
+python_image_version='v2'
+opensafely pull "python:$python_image_version"
+
+docker_python_packages=$(docker run "ghcr.io/opensafely-core/python:$python_image_version" python -m pip freeze)
+local_python_packages=$(/opt/venv/bin/python3.10 -m pip freeze)
+diff <(echo "$local_python_packages") <(echo "$docker_python_packages")
+
 # Check the RStudio server is running.
 curl -L 'http://localhost:8787' | grep 'RStudio'
 
